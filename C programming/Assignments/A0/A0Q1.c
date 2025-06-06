3037aff72a08328dc9104efd6459fe9d6e0505ee
vasi-583 <vk4017227@gmail.com>
2025-06-06 22:25:25 +0530

    Add files via upload

---
+++ C programming/Assignments/A0/A0Q1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int no1,no2, res;
+	printf("Enter the first num :");
+	scanf("%d",&no1);
+	
+	printf("Enter the second num:");
+	scanf("%d", &no2);
+	
+	res=no1+no2;
+	
+	printf("sum of %d and %d is %d\n", no1 , no2 , res);
+}
